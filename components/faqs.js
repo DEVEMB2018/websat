8dc6551b7d43241e03fccd11ecec280d4b2770c9
Daniel García <daniel@etereo.io>
2018-10-02 11:04:17 +0200

    feature(faqs): add some faqs texts

--- components/faqs.js
+++ components/faqs.js
@@ -8,27 +8,35 @@ import caretStyles from '../styles/components/caret.scss'
 
 const faqs = [
   {
-    title: '¿Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
-    content: 'Maecenas mollis odio lectus, nec condimentum orci ultricies quis. Vivamus fermentum, libero non interdum dignissim, nulla diam aliquam enim, cursus interdum dolor felis quis arcu. Maecenas nec magna commodo, facilisis turpis sit amet, maximus arcu. Vivamus at scelerisque eros.'
+    title: '¿Qué necesito para tener internet por satélite?',
+    content: 'El servicio Internet Satélite requiere de una parabólica y un modem. Embou se encarga de proveer, instalar y orientar ambos equipos en tu hogar para que tú no tengas que preocuparte de nada. La parabólica se instala en el exterior de tu hogar y se conecta por cable al router wifi, que se instalará en el interior de tu hogar. Estos equipos se proporcionarán en modalidad de alquiler.'
   },
   {
-    title: '¿Nulla porttitor leo nulla, et facilisis tellus molestie a?',
+    title: '¿Hasta cuando dura la promoción de alta gratis?',
     content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Vivamus vestibulum nec eros in efficitur.'
   },
   {
-    title: '¿Duis pretium augue sed metus venenatis, non semper lorem malesuada?',
+    title: '¿Puedes disponer de telefonía fija?',
     content: 'Donec quis eros sit amet nibh tempus maximus ac vel purus. Curabitur vel erat fringilla, vehicula augue vel, interdum magna. Aliquam erat volutpat. Cras eget accumsan arcu.'
   },
   {
-    title: '¿Proin et risus finibus, suscipit arcu sed, luctus odio?',
+    title: '¿Puedo consultar el consumo de mi conexión?',
     content: 'Praesent gravida placerat odio, eu pellentesque nisi cursus sed. In fermentum, purus a finibus pulvinar, purus sem mollis massa, et egestas lorem massa ut turpis. Morbi dui metus, mollis ac leo sit amet, tincidunt sagittis dui. Integer ut purus ex.'
   },
   {
-    title: '¿Cras faucibus egestas enim, tincidunt sagittis nisl dictum ut?',
+    title: '¿El tráfico de subida se descuenta del volumen de datos contratados?',
     content: 'Praesent gravida placerat odio, eu pellentesque nisi cursus sed. In fermentum, purus a finibus pulvinar, purus sem mollis massa, et egestas lorem massa ut turpis. Morbi dui metus, mollis ac leo sit amet, tincidunt sagittis dui. Integer ut purus ex.'
   },
   {
-    title: '¿Cras faucibus egestas enim, tincidunt sagittis nisl dictum ut. Proin et risus finibus??',
+    title: '¿Por qué se me agotan rápido los datos?',
+    content: 'Praesent gravida placerat odio, eu pellentesque nisi cursus sed. In fermentum, purus a finibus pulvinar, purus sem mollis massa, et egestas lorem massa ut turpis. Morbi dui metus, mollis ac leo sit amet, tincidunt sagittis dui. Integer ut purus ex.'
+  },
+  {
+    title: '¿Cuando se resetea mi consumo de datos contratados?',
+    content: 'Praesent gravida placerat odio, eu pellentesque nisi cursus sed. In fermentum, purus a finibus pulvinar, purus sem mollis massa, et egestas lorem massa ut turpis. Morbi dui metus, mollis ac leo sit amet, tincidunt sagittis dui. Integer ut purus ex.'
+  },
+  {
+    title: '¿Puedo disfrutar de servicios multimedia con mi conexión de internet por satélite?',
     content: 'Praesent gravida placerat odio, eu pellentesque nisi cursus sed. In fermentum, purus a finibus pulvinar, purus sem mollis massa, et egestas lorem massa ut turpis. Morbi dui metus, mollis ac leo sit amet, tincidunt sagittis dui. Integer ut purus ex.'
   }
 ].map((feature, index) => {
@@ -43,7 +51,7 @@ class Faqs extends React.Component {
     return <div className={faqsStyles.container}>
       <div className={faqsStyles.titleContainer}>
         <h1 className={faqsStyles.title}>Preguntas frecuentes</h1>
-        <p className={faqsStyles.subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis odio lectus, nec condimentum orci ultricies quis</p>
+        <p className={faqsStyles.subtitle}>Aquí respondemos a las preguntas más habituales de nuestros clientes antes de contratar</p>
       </div>
       <div className={faqsStyles.faqsContainer}>
         {faqs.map((faq) => (
